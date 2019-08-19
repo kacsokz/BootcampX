de9b43aed80d74c6f9ae126feb1b99ace1290f69
kacsokz <casey.sokach@gmail.com>
2019-08-19 21:02:18 +0000

    Add more queries

---
+++ 3_queries_group-by/12_large_cohorts.sql
@@ -0,0 +1,8 @@
+SELECT
+  cohorts.name AS cohort_name,
+  count(students.*) AS student_count
+FROM cohorts
+JOIN students ON cohort_id = cohorts.id
+GROUP BY cohort_name
+HAVING count(students.*) >= 18
+ORDER BY student_count;
---
+++ 3_queries_group-by/14_average_completion_time.sql
@@ -0,0 +1,8 @@
+SELECT
+  students.name AS student,
+  avg(assignment_submissions.duration) AS average_assignment_duration
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+ORDER BY average_assignment_duration DESC;
