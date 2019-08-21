d11f3b3b76c1b2c31fadf1097a8a926128ece265
kacsokz <casey.sokach@gmail.com>
2019-08-21 23:13:11 +0000

    Santize CLI input

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT
-    DISTINCT teachers.name AS teacher,
-    cohorts.name AS cohort
-  FROM teachers
-  JOIN assistance_requests ON teachers.id = teacher_id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '${process.argv[2]}%'
-  ORDER BY teachers.name;
-`)
+const queryString = `
+SELECT
+  DISTINCT teachers.name AS teacher,
+  cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;
+`;
+
+const cohortName = process.argv[2];
+const values = [`${cohortName}`]
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
